d6d2708dd7f7a74dc6ad44d097f4d5c76be69498
ralph.yu <ralph.yu@cloudminds.com>
2020-12-17 20:34:37 +0000

    code transfer

---
+++ __config__.py
@@ -0,0 +1,12 @@
+# Harix
+server_ip = '10.128.0.254:32483'
+kafka_topic = 'detect_record'
+
+
+# Third-party cloud
+
+# client_id = '34a002e0-c7f8-11ea-a67f-15e30d90bbf4'
+# username = '17a1d4c0-c7f8-11ea-93bf-d33a96695544'
+# password = '51ac8a2cb843e7feddd7bc24b262078c26811b7e'
+# IMEI = 862851030069754
+
