9fbfe7ef325bcc815f642da68aaa6a9115063bf5
Baoqiang.Du <baoqiang.du@cloudminds.com>
2020-12-22 09:09:15 +0800

    no message

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
 
-engine = create_engine('mysql://root:111111@localhost/contacts',echo=False)
+engine = create_engine('mysql://root:cloud1688@kafka-thirdparty-mysql:3306/contacts',echo=False)
 if not database_exists(engine.url):
     create_database(engine.url)
 
@@ -11,5 +11,5 @@ print(database_exists(engine.url))
 df = pd.read_csv('database.csv')
 df.to_sql('infos',if_exists='replace',con=engine)
 
-new_df = pd.read_sql_table('infos','mysql://root:111111@localhost/contacts', index_col = 'Device_Name')
+new_df = pd.read_sql_table('infos','mysql://root:cloud1688@kafka-thirdparty-mysql:3306/contacts', index_col = 'Device_Name')
 new_df = new_df.drop(columns=['index'])
